e007816e53cdfbdc5b644ac5d590a63539cd2ab9
masakinihirota <masakinihirota@gmail.com>
2023-06-04 21:39:50 +0900

    🎨 コードの構造/形式を改善します。 Navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import SupabaseListener from "./components/supabase-listener"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
   title: "Supabase Auth",
@@ -14,6 +15,8 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
     <html>
       <body className={inter.className}>
         <div className="flex flex-col min-h-screen">
+          <SupabaseListener />
+
           <main className="flex-1 container max-w-screen-sm mx-auto px-1 py-5">{children}</main>
 
           <footer className="py-5">
